c5e85cb5411b7fc30b40c8d572edb242e5fcab01
Mehmet Said Unlu <mehmetkatza2001@gmail.com>
2024-05-30 22:04:17 +0200

    MyFirstWebSite

---
+++ Script.js
@@ -0,0 +1,5 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('contactButton').addEventListener('click', function () {
+        window.location.href = 'contact.html';
+    });
+});
